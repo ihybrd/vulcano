match camera position & rotation when switch to WASD camera, add normal/tangent info when loading obj with assimp, fix the joystick bug when joystick is not presented.

app/graphics/engine.py
@@ -40,8 +40,6 @@ class Engine:
         self.camera = camera.Camera()
         self.camera_wasd = camera.CameraWasd()
 
-        self.use_wasd_cam = True
-    
     def make_instance(self):
 
         self.instance = instance.make_instance("")
@@ -314,10 +312,6 @@ class Engine:
 
         _frame: frame.SwapChainFrame = self.swapchainFrames[imageIndex]
 
-        position = np.array([1, 0, -1],dtype=np.float32)
-        target = np.array([0, 0, 0],dtype=np.float32)
-        up = np.array([0, 0, -1],dtype=np.float32)
-        # _frame.cameraData.view = pyrr.matrix44.create_look_at(position, target, up, dtype=np.float32)
         self.input.update()
 
         if self.input.keyboard_handler.mode:
@@ -326,6 +320,9 @@ class Engine:
         else:
             self.camera.update(self.input.mouse_handler)
             view = self.camera.view()
+            # set wasd cam stat, todo: this should be done only the moment when the mode is changed
+            self.camera_wasd.set_pos(self.camera.pos())
+            self.camera_wasd.set_rot(self.camera.rx, self.camera.ry)
 
         _frame.cameraData.view = view
 

app/resource_manager/asset.py
@@ -18,8 +18,8 @@ class Asset:
         m = scene.meshes[0]
 
         vertices = []
-        for i in m.vertices:
-            vertices += list(i)+[1,1,1]+[0,0] # vert, color, uv
+        for id, verts in enumerate(m.vertices):
+            vertices += list(verts)+[1,1,1]+list(m.normals[id])+list(m.tangents[id])+[0,0] # vert, color, uv
         indices = []
         for i in m.indices:
             indices += list(i)
@@ -54,8 +54,8 @@ class Asset:
 
     def load_star(self):
         if self.use_mesh:
-            # return self.load_mesh_assimp()
-            return self.load_mesh_OBJimporter()
+            return self.load_mesh_assimp()
+            # return self.load_mesh_OBJimporter()
         else:
             vertices = np.array(
                 (